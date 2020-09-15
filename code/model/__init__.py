fa0a0412176ef77ad7f6fcb95d5dd8e5238b5a45
lewisyangliu <lewisyangliu@gmail.com>
2020-09-15 22:13:28 +0800

    Create __init__.py

---
+++ code/model/__init__.py
@@ -0,0 +1,96 @@
+import os
+from importlib import import_module
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+class Model(nn.Module):
+    def __init__(self, args, ckp):
+        super(Model, self).__init__()
+        print('Making model...')
+
+        self.precision = args.precision
+        self.cpu = args.cpu
+        self.device = torch.device('cpu' if args.cpu else 'cuda')
+        self.n_GPUs = args.n_GPUs
+        self.save_models = args.save_models
+
+        module = import_module('model.' + args.model.lower())
+        self.model = module.make_model(args).to(self.device)
+        if args.precision == 'half': self.model.half()
+
+        if not args.cpu and args.n_GPUs > 1:
+            self.model = nn.DataParallel(self.model, range(args.n_GPUs))
+
+        self.load(
+            ckp.dir,
+            pre_train=args.pre_train,
+            resume=args.resume,
+            cpu=args.cpu
+        )
+        if args.print_model: print(self.model)
+
+    def forward(self, x):
+        
+        return self.model(x)
+
+    def get_model(self):
+        if self.n_GPUs == 1:
+            return self.model
+        else:
+            return self.model.module
+
+    def state_dict(self, **kwargs):
+        target = self.get_model()
+        return target.state_dict(**kwargs)
+
+    def save(self, apath, epoch, is_best=False):
+        target = self.get_model()
+        torch.save(
+            target.state_dict(), 
+            os.path.join(apath, 'model', 'model_latest.pt')
+        )
+        if is_best:
+            torch.save(
+                target.state_dict(),
+                os.path.join(apath, 'model', 'model_best.pt')
+            )
+        
+        if self.save_models:
+            torch.save(
+                target.state_dict(),
+                os.path.join(apath, 'model', 'model_{}.pt'.format(epoch))
+            )
+
+    def load(self, apath, pre_train='.', resume=-1, cpu=False):
+        if cpu:
+            kwargs = {'map_location': lambda storage, loc: storage}
+        else:
+            kwargs = {}
+
+        if resume == -1:
+            print('Resuming model from {}'.format('the latest'))
+            self.get_model().load_state_dict(
+                torch.load(
+                    os.path.join(apath, 'model', 'model_latest.pt'),
+                    **kwargs
+                ),
+                strict=False
+            )
+        elif resume == 0:
+            if pre_train != '.':
+                print('Loading model from {}'.format(pre_train))
+                self.get_model().load_state_dict(
+                    torch.load(pre_train, **kwargs),
+                    strict=False
+                )
+        else:
+            self.get_model().load_state_dict(
+                torch.load(
+                    os.path.join(apath, 'model', 'model_{}.pt'.format(resume)),
+                    **kwargs
+                ),
+                strict=False
+            )
+
